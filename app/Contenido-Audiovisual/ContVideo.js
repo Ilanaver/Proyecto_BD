0640e6c89e555c3452cc6896a1c4871d88628ea5
FRANCO PASQUALE SANGUINETTI <47573419@ort>
2024-08-05 10:06:35 -0300

    definicion de terminos terminado

---
+++ app/Contenido-Audiovisual/ContVideo.js
@@ -0,0 +1,63 @@
+import style from './contenidoaudiovisual.module.css';
+
+
+const video = () => {
+    <div class="Titulo">
+        <h1>Contenido Audiovisual</h1>
+        <div class="perfil">
+        </div>
+    </div>
+    <div class="contenedorContenido">
+        <div class="contenido">
+            <div class="tituloContenido">
+                <h2>Ahorro</h2>
+                <a href="">Ver mas</a>
+            </div>
+            <div class="imagenesContenido">
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+            </div>
+        </div>
+        <div class="contenido">
+            <div class="tituloContenido">
+                <h2>Invertir</h2>
+                <a href="">Ver mas</a>
+            </div> 
+            <div class="imagenesContenido">
+                <img src="../assets/img/Audiovizual.png" alt="">
+                <img src="../assets/img/Audiovizual.png" alt="">
+                <img src="../assets/img/Audiovizual.png" alt="">
+                <img src="../assets/img/Audiovizual.png" alt="">
+            </div>
+        </div>
+        <div class="contenido">
+            <div  class="tituloContenido">
+                <h2>Impuestos</h2>
+                <a href="">Ver mas</a>
+            </div>
+            <div class="imagenesContenido">
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+                <img src="../assets/img/Audiovizual.png" alt=""/>
+            </div>
+        </div>
+        <div class="contenido">
+            <div class="tituloContenido">
+                <h2>Ahorro</h2>
+                <a href="">Ver mas</a>
+            </div>
+            <div class="imagenesContenido">
+                <img src="../assets/img/Audiovizual.png" alt="">
+                <img src="../assets/img/Audiovizual.png" alt="">
+                <img src="../assets/img/Audiovizual.png" alt="">
+                <img src="../assets/img/Audiovizual.png" alt="">
+            </div>
+            
+        </div>
+        
+    </div>
+}
+export default video;
