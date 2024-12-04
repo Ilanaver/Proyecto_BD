8427f41a046176bff7a3714d908d4eeeeb9932a1
ilanaver <ilan_correo@averbuch.com.ar>
2024-12-04 12:14:16 -0300

    get user id listo

--- app/page.js
+++ app/page.js
@@ -1,10 +1,32 @@
 'use client';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation"; // Para redirigir programáticamente
 import styles from './page.module.css'; // Asegúrate de que el archivo de estilos esté en la misma ubicación o ajusta la ruta si es necesario.
 
 const Home = () => {
-  return (
-    <>
+  const router = useRouter();
+  const [loading, setLoading] = useState(true); // Estado para manejar la pantalla de carga
+
+  useEffect(() => {
+    // Simula una pantalla de carga mientras verificas el localStorage
+    const timer = setTimeout(() => {
+      const userId = localStorage.getItem('userId');
+      if (userId) {
+        // Si hay un userId en el localStorage, redirige a la pantalla principal
+        router.push('/Gestor');
+      } else {
+        // Si no hay userId, redirige a la pantalla de inicio de sesión
+        router.push('/iniciosesion');
+      }
+    }, 1000); // Tiempo de espera para simular la carga
+
+    // Limpiar el temporizador en caso de desmontar el componente
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  // Mientras se verifica el localStorage, muestra una pantalla de carga
+  if (loading) {
+    return (
       <section>
         <div className={styles.PaginaPrincipal}>
           <div className={styles.contenedorParteArriba}>
@@ -13,25 +35,17 @@ const Home = () => {
             </div>
             <div className={styles.contenedorTexto}>
               <h2>
-                <span className={styles.palabrasInicio}>Aprendé</span>, <span className={styles.palabrasInicio}>gestioná</span> y{' '}
-                <span className={styles.palabrasInicio}>consultá</span> en un mismo lugar
+                <span className={styles.palabrasInicio}>Cargando</span> tu experiencia...
               </h2>
-            </div>
-            <div className={styles.contenedorInicioSesion}>
-              <div className={styles.botonRegistrarse}>
-                <button type="submit" onClick={() => window.location.href = '/registro'}>
-                  Registrarse
-                </button>
-              </div>
-              <div className={styles.contenedorP}>
-                <a href="./iniciosesion">¿Ya tenés una cuenta? <br /> InicioSesion</a>
-              </div>
             </div>
           </div>
         </div>
       </section>
-    </>
-  );
+    );
+  }
+
+
+  return null; // No renderiza nada porque siempre redirige
 };
 
 export default Home;
