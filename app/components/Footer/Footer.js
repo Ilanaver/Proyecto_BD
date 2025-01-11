77137b5a2d9998446c02785fa75a09018b5c00d2
ilanaver <ilan_correo@averbuch.com.ar>
2025-01-11 17:36:15 -0300

    un poco de la leccion diaria, necesita mejorar.

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
             <nav className={styles.navegador}>
                 <ul className={styles.footerPagina}>
                     <li className={styles.navegadorFooter}>
-                        <a href="../Gestor"><img src="/gestor.png" alt="Foto Academia"/></a>Gestor
+                        <a href="../Gestor"><img src="/gestor.png" alt="Foto Academia"/></a>
                     </li>
                     <li className={styles.navegadorFooter}>
-                        <a href="../academia"><img src="/academia.png" alt="Foto Academia"/></a>Academia
+                        <a href="../academia"><img src="/academia.png" alt="Foto Academia"/></a>
                     </li>
                     <li className={styles.navegadorFooter}>
-                        <a href="../estadisticas"><img src="/estadisticas.png" alt="Foto Estadisticas"/></a>Estadisticas
+                        <a href="../estadisticas"><img src="/estadisticas.png" alt="Foto Estadisticas"/></a>
                     </li>
                 </ul>
             </nav>
